7500079ae9706d2d4f0fa28a262d198fb3fc8526
aubrey-y <aubreym.yan@gmail.com>
2021-02-19 20:55:07 -0500

    added hover animation for label buttons

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -2,10 +2,19 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
+
     @FXML
-    private void switchToSecondary() throws IOException {
+    private Button primaryButton;
+
+    @FXML
+    private Label startLabel;
+
+    @FXML
+    private void switchToSecondary() throws IOException, InterruptedException {
         App.setRoot("secondary");
     }
 
