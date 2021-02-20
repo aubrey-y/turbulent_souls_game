7ed86a0575a7856ad2215e6cefeeba99403d1c35
aubrey-y <aubreym.yan@gmail.com>
2021-02-20 01:41:11 -0500

    added music player

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.media;
 
     opens org.example to javafx.fxml;
     opens org.example.controllers to javafx.fxml;
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * JavaFX App
@@ -13,10 +16,18 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+
+    private MediaPlayer mediaPlayer;
+
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 1920, 1080);
 //        scene.getStylesheets().add(getClass().getResource("/static/fontstyle.css").toExternalForm());
+
+        Media media = new Media(Paths.get("src/main/resources/static/music/bardsadventure.mp3").toUri().toString());
+        this.mediaPlayer = new MediaPlayer(media);
+        this.mediaPlayer.play();
+
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.setFullScreen(true);
