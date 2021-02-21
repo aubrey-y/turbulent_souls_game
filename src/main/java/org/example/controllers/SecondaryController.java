ac6d4e87c9b0b750ea8d52396866e89c8cd47c4d
Jimmy <jpham64@gatech.edu>
2021-02-21 01:01:38 -0500

    added button rotate

--- src/main/java/org/example/controllers/SecondaryController.java
+++ src/main/java/org/example/controllers/SecondaryController.java
@@ -1,18 +1,38 @@
 package org.example.controllers;
 
 import java.io.IOException;
+
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.example.App;
 
 public class SecondaryController {
+
+    @FXML
+    private TextField nameID;
+    private Button startButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
 
+    /**
     @FXML
     private void switchToGameScreen() throws IOException {
         App.setRoot("gameScreen");
     }
+     */
+
+    @FXML
+    void switchToGameScreen(ActionEvent event) {
+
+    }
+
+
 }
